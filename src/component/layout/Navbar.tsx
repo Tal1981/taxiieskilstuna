2ee5795cc5d3c68f9553399b2031e5f06da7f2e7
Tal1981 <orange.2009@hotmail.com>
2024-10-25 16:40:29 +0100

    change on css

--- src/component/layout/Navbar.tsx
+++ src/component/layout/Navbar.tsx
@@ -7,6 +7,8 @@ import Img from "@/../public/logo.png";
 function Navbar({darkMode,toggleDarkMode }:{darkMode:boolean, toggleDarkMode: () => void}) {
     const isTop : boolean = UseScroll();
 
+    console.log(UseScroll())
+
     return (
         <section id="navbar" className={`sticky -mt-[8.1rem] top-0 z-40 p-2 ${isTop? 'opacity-100':'opacity-0 hover:opacity-100 transition-opacity duration-500'} flex flex-col flex-nowrap justify-around items-start h-fit w-full text-white ${darkMode ? 'bg-gray-900' : 'bg-gradient-to-r from-transparent to-darkColor text-shadow'} `}>
             <div className={`flex justify-between items-center h-[60px] w-full px-4`}>
