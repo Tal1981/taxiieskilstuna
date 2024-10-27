07ca9802956a3ac5c87c9dff6653ff942fd757b7
Tal1981 <orange.2009@hotmail.com>
2024-10-27 02:36:06 +0000

    test on css with calender

--- src/component/form/FlatpickrComp.tsx
+++ src/component/form/FlatpickrComp.tsx
@@ -22,7 +22,8 @@ const FlatpickrComp: React.FC = () => {
                     name="date"
                     required
                     // className="absolute top-0 left-0 w-full p-2 border text-black z-10 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    
+                    className="focus:outline-0"
+                    style={{background:"transparent",width:"100%",height:"100%",borderWidth:"0"}}
                 />    
     );
 };
