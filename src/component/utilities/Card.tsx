a9acff1dc03439f31f23bd19ebcc1c06d5a3eff8
Tal1981 <orange.2009@hotmail.com>
2024-10-27 10:54:17 +0000

    add overflow-x: hidden to html in  globals.css & position:fixed to navbar.tsx

--- src/component/utilities/Card.tsx
+++ src/component/utilities/Card.tsx
@@ -5,8 +5,8 @@ const Card = (children: { destination: string, prise: string, old_prise: string 
     
 
     return (
-        <div className="w-full max-w-sm p-4 border-2 border-orange-300 rounded-lg shadow sm:p-8">
-            <h5 className="mb-4 text-xl font-medium text-nowrap">Eskilstuna ⇔ {children.destination}</h5>
+        <div className="w-full max-w-sm p-3 border-2 border-orange-300 rounded-lg shadow sm:p-8">
+            <h5 className="mb-4 text-xl font-medium text-nowrap m-0 p-0">Eskilstuna ⇔ {children.destination}</h5>
             <div className="flex items-baseline">
                 <span className="prise text-5xl font-extrabold tracking-tight">{children.prise}</span>
                 <span className="text-xl font-normal ms-1">SEK</span>
