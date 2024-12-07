46e12e81e2a6716650f6d70f85d5ca2d4557e072
Tal1981 <orange.2009@hotmail.com>
2024-12-07 22:48:09 +0000

    moved <a href='tel:+46762259996'> from Kontakt.tsx to Hero.tsx wafi asked for that

--- src/component/layout/Hero.tsx
+++ src/component/layout/Hero.tsx
@@ -11,8 +11,14 @@ function Hero({darkMode}:{darkMode:boolean}) {
     return (
         <section className="w-full" >
             <div id="hero" className="min-h-fit px-8 sm:px-12 md:px-20 pt-[10rem] pb-7 bg-gold relative flex  flex-col  sm:flex-row justify-center sm:justify-between items-center gap-2">
-                <div className="w-full md:w-2/5 flex flex-col items-center z-20 pointer-events-none mt-7 sm:-mt-2 gap-4">
+                <div className="w-full md:w-2/5 flex flex-col items-center z-20 pointer-events-none mt-7 sm:-mt-2 gap-3">
                     <Typing/>
+                    <a href="tel:+46762259996" className="z-50">
+                        <button className="relative bg-black text-white text-lg font-bold w-fit py-1 px-4 rounded hover:bg-darkColor transition duration-300 pointer-events-auto border-b-4 border-orange-500 tracking-widest">
+                            <svg className="absolute -top-3 -left-3 h-6 w-6 bg-white p-1 rounded-full text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" /></svg>
+                            +46 76 225 99 96
+                        </button>
+                    </a>
                     <button className="relative bg-black text-white font-bold w-fit py-2 px-5 rounded hover:bg-darkColor transition duration-300 pointer-events-auto border-b-4 border-orange-500">
                         <span className="relative inline-flex rounded-full h-3 w-3 bg-gold">
                             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-50 opacity-70"></span>
--- src/component/layout/Kontakt.tsx
+++ src/component/layout/Kontakt.tsx
@@ -8,12 +8,6 @@ function Kontakt() {
 
     return (
     <section id="" className="flex flex-col justify-center items-center h-fit pb-20 px-8 mt-0 pt-0"> 
-        <a href="tel:+46762259996" className="z-50 mb-12">
-            <button className="relative bg-black text-white text-lg font-bold w-fit py-1 px-4 rounded hover:bg-darkColor transition duration-300 pointer-events-auto border-b-4 border-orange-500 tracking-widest">
-                <svg className="absolute -top-3 -left-3 h-6 w-6 bg-white p-1 rounded-full text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" /></svg>
-                +46 76 225 99 96
-            </button>
-        </a>
 
         <p className="text-center text-xl tracking-widest">Boka & beställ taxi på telefon +46-762259996 eller här online</p>
         <p className=" text-center text-base tracking-widest mt-4">
